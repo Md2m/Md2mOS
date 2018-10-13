d1ea5b40678f86f6efaa5f29c993c036629bb642
md2m <md2m70@gmail.com>
2018-10-13 22:33:27 +0330

    change kernel.c file for test

--- kernel.c
+++ kernel.c
@@ -3,11 +3,11 @@ kmain(){
 
 char* vidmem = (char*) 0xb8000;
 vidmem[0] = 'M';
-vidmem[1] = 0x05;
+vidmem[1] = 0x04;
 vidmem[2] = 'D';
 vidmem[3] = 0x05;
 vidmem[4] = '2';
-vidmem[5] = 0x05;
+vidmem[5] = 0x04;
 vidmem[6] = 'M';
 vidmem[7] = 0x05;
 
